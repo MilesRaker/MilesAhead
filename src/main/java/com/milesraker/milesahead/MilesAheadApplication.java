139e002ae811dac3a55c433e935cd5ffdfad696d
Miles Raker <milesraker@gmail.com>
2022-12-31 18:34:12 -0800

    Followed project setup from Codeup curriculum. Added version number to pom: spring-boot-maven-plugin.

---
+++ src/main/java/com/milesraker/milesahead/MilesAheadApplication.java
@@ -0,0 +1,13 @@
+package com.milesraker.milesahead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilesAheadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MilesAheadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/milesraker/milesahead/MilesAheadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milesraker.milesahead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilesAheadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
