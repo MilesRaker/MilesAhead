eb75702f81aa178c5494ef228a17f6a56d823cf1
Miles Raker <milesraker@gmail.com>
2023-01-03 14:21:17 -0800

    used th:each and th:text to dynamically create pages for the posts

--- src/main/java/com/milesraker/milesahead/controllers/PostController.java
+++ src/main/java/com/milesraker/milesahead/controllers/PostController.java
@@ -1,19 +1,31 @@
 package com.milesraker.milesahead.controllers;
 
+import com.milesraker.milesahead.Classes.Post;
 import org.apache.coyote.Request;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
     @GetMapping("/posts")
-    public String posts() {
-
+    public String posts(Model model) {
+        Post post1 = new Post(1, 1, "Too Many Cats!", "Please take some of these cats. There are so so many. I really need to get these cute kittens into new homes.");
+        Post post2 = new Post(2, 1, "Not Enough Lizards", "Send me Lizards! Trade lizards for cats? I like the beareded ones.");
+        List<Post> posts = new ArrayList<>();
+        posts.add(post1);
+        posts.add(post2);
+        model.addAttribute("posts", posts);
         return "/posts/index";
     }
 
     @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
-    public String individualPost(@PathVariable int id) {
+    public String individualPost(Model model) {
+        Post post1 = new Post(1, 1, "Too Many Cats!", "Please take some of these cats. There are so so many. I really need to get these cute kittens into new homes.");
+        model.addAttribute("post", post1);
         return "/posts/post";
     }
 
